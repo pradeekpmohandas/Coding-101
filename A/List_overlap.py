157e7b66e25ca01901ebaebb7d93132032bfb2ff
pradeek p mohandas <pradeek.pm@gmail.com>
2018-03-13 16:41:00 +0530

    Update List_overlap.py

--- A/List_overlap.py
+++ A/List_overlap.py
@@ -1,3 +1,4 @@
+#method 1
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 r = []
@@ -8,3 +9,19 @@ for number_a in a:
             if(number_a not in r):
               r.append(number_a)
 print ("The commmon values: ", r)
+
+#method 2
+a = [1, 1, 2, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = set(a) & set(b)
+print(c)
+
+
+#method 3 
+a = [1, 1, 2, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+for i in a:
+    if i in b and i not in c:
+        c.append(i)
+print(c)
