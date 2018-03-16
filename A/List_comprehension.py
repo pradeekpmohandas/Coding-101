83d4c38cee1e824c1978d0de519f0ffd1f0ae279
pradeek p mohandas <pradeek.pm@gmail.com>
2018-03-16 12:05:16 +0530

    Update List_comprehension.py

--- A/List_comprehension.py
+++ A/List_comprehension.py
@@ -22,3 +22,20 @@ import random
 a = random.sample(range(1,30), 12)
 b = random.sample(range(1,30), 16)
 result = [i for i in set(a) if i in b]
+
+
+#prime check using list comprehension 
+
+num = int(raw_input('Insert a number: '))
+a = [x for x in range(2, num) if num % x == 0]
+
+def is_prime(n):
+	if num > 1:
+		if len(a) == 0:
+			print 'prime'
+		else:
+			print 'NOT prime'
+	else:
+		print 'NOT prime'
+	
+is_prime(num)
