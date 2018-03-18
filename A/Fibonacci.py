c529e3a967b60f7d138375ab11d343351dcadc5c
pradeek p mohandas <pradeek.pm@gmail.com>
2018-03-18 20:01:27 +0530

    Update Fibonacci.py

--- A/Fibonacci.py
+++ A/Fibonacci.py
@@ -14,3 +14,34 @@ def printFibno(n):
   return fib 
 
 print(printFibno(int(input("Enter Number"))))
+
+
+#another code Fibonacci Numbers
+def printFibonacciNumbers(n):
+     
+    f1 = 0
+    f2 = 1
+    if (n < 1):
+        return
+    for x in range(0, n):
+        print(f2, end = " ")
+        next = f1 + f2
+        f1 = f2
+        f2 = next
+printFibonacciNumbers(7)
+ 
+#Recussrsive Solution :
+# Function for nth Fibonacci number
+ 
+def Fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n==1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n==2:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+      
